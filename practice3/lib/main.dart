74d221ada5b3e2c54a44e659fd09e388610d2800
minseok128 <minseok128128@gmail.com>
2023-01-08 22:04:33 +0900

    Initial Practice3

---
+++ practice3/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      home: const Scaffold(
+        body: Center(child: Text('HELLO')),
+      ),
+    );
+  }
+}
