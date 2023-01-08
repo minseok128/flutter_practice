78bc49e1f5f9cb365a3cd13ff029cd33fa641049
minseok128 <minseok128128@gmail.com>
2023-01-08 18:48:32 +0900

    update Counter

--- practice2/lib/main.dart
+++ practice2/lib/main.dart
@@ -12,16 +12,18 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int counter = 0;
+  List<int> numbers = [];
   void onClickedAdd() {
     setState(() {
-      counter++;
+      numbers.add(numbers.length);
     });
   }
 
   void onClickedSub() {
     setState(() {
-      counter--;
+      if (numbers.isNotEmpty) {
+        numbers.removeLast();
+      }
     });
   }
 
@@ -29,51 +31,78 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
       home: Scaffold(
         backgroundColor: Colors.indigo,
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
-                'Click Counter',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 30,
-                ),
-              ),
-              Text(
-                '$counter',
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 40,
-                ),
-              ),
-              const SizedBox(
-                height: 60,
+              const MyLargeTitle(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  for (var n in numbers)
+                    SizedBox(
+                      width: 20,
+                      height: 20,
+                      child: Text(
+                        '$n',
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                ],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   IconButton(
-                      onPressed: onClickedAdd,
-                      icon: const Icon(
-                        Icons.add_box,
-                        color: Colors.white,
-                        size: 30,
-                      )),
+                    onPressed: onClickedAdd,
+                    icon: const Icon(
+                      Icons.add_circle_outline,
+                      color: Colors.white,
+                      size: 30,
+                    ),
+                  ),
                   IconButton(
-                      onPressed: onClickedSub,
-                      icon: const Icon(
-                        Icons.indeterminate_check_box,
-                        color: Colors.white,
-                        size: 30,
-                      )),
+                    onPressed: onClickedSub,
+                    icon: const Icon(
+                      Icons.remove_circle_outline,
+                      color: Colors.white,
+                      size: 30,
+                    ),
+                  ),
                 ],
-              )
+              ),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Counter',
+      style: TextStyle(
+        color: Theme.of(context).textTheme.titleLarge?.color,
+        fontSize: 30,
       ),
     );
   }
