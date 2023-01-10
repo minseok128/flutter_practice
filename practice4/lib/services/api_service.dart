2654d21d63c2efbcbe7791a68decfa361eeed48d
minseok128 <minseok128128@gmail.com>
2023-01-10 20:11:56 +0900

    Update API

--- practice4/lib/services/api_service.dart
+++ practice4/lib/services/api_service.dart
@@ -1,15 +1,23 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:practice4/models/webtoon.dart';
 
 class ApiService {
+  List<WebtoonModel> toonInstances = [];
   final String baseURL = 'https://webtoon-crawler.nomadcoders.workers.dev';
   final String today = "today";
 
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
     final url = Uri.parse('$baseURL/$today');
     final response = await http.get(url);
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        final toon = WebtoonModel.fromJson(webtoon);
+        toonInstances.add(toon);
+      }
+      return toonInstances;
     }
     throw Error();
   }
